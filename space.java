2db79c2c5d2de3592f9f4cbca823721ff928065a
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-19 15:15:51 +0530

    Create space.java

---
+++ space.java
@@ -0,0 +1,14 @@
+package janani;
+import java.util.Scanner;
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,n1;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+n1=ja.nextInt();
+System.out.println(n+""+n1);
+}
+}
